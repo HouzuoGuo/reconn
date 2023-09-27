6b1f2104299c4ae568418eb05ca035dc81a65e42
HouzuoGuo <guohouzuo@gmail.com>
2023-09-27 19:25:26 +0100

    voicesvc: complete the first iteration of voice clone endpoint

--- voicesvc/clone.py
+++ voicesvc/clone.py
@@ -1,16 +1,22 @@
 import logging
+import numpy
 import torchaudio
+import torch
 import os
 
 from encodec.utils import convert_audio
 from svc import VoiceSvc
-from conf import CONFIG_VOICE_SAMPLE_KEY
+from conf import (
+    CONFIG_VOICE_SAMPLE_KEY,
+    CONFIG_AI_COMPUTING_DEVICE,
+    CONFIG_VOICE_MODEL_KEY,
+)
 from flask import jsonify, request, Flask
 
 
 # Clone the user's voice using the input wave sample. This is a synchronous handler.
 def clone_rt_handler(app: Flask, svc: VoiceSvc):
-    @app.route("/clone-sync/<user_id>", methods=["POST"])
+    @app.route("/clone-rt/<user_id>", methods=["POST"])
     def clone_rt_handler(user_id: str):
         if request.content_type not in ["audio/x-wav", "audio/wav", "audio/wave"]:
             return "", 406
@@ -27,6 +33,22 @@ def clone_rt_handler(app: Flask, svc: VoiceSvc):
             wav, sr, svc.bark_codec_model.sample_rate, svc.bark_codec_model.channels
         )
         wav = wav.to(app.config[CONFIG_AI_COMPUTING_DEVICE])
+        semantic_vectors = svc.hubert_model.forward(
+            wav, input_sample_hz=svc.bark_codec_model.sample_rate
+        )
+        semantic_tokens = svc.hubert_tokeniser.get_token(semantic_vectors)
+        with torch.no_grad():
+            encoded_frames = svc.bark_codec_model.encode(wav.unsqueeze(0))
+        codes = torch.cat([encoded[0] for encoded in encoded_frames], dim=-1).squeeze()
+        codes = codes.cpu().numpy()
+        semantic_tokens = semantic_tokens.cpu().numpy()
+        output_path = os.path.join(app.config[CONFIG_VOICE_MODEL_KEY], user_id + ".npz")
+        numpy.savez(
+            output_path,
+            fine_prompt=codes,
+            coarse_prompt=codes[:2, :],
+            semantic_prompt=semantic_tokens,
+        )
         return jsonify(
             {
                 "request-method": request.method,
